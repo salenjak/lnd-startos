d55266780404f56969e42455fadb471437da7dc7
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-19 22:19:10 -0700

    bump pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
-export { v0_19_3_1_beta_2 as current } from './v0.19.3-beta_1-beta.2'
+export { v0_19_3_1_beta_3 as current } from './v0.19.3-beta_1-beta.3'
 import { v0_19_3_1_beta_0 } from './v0.19.3-beta_1-beta.0'
 import { v0_19_3_1_beta_1 } from './v0.19.3-beta_1-beta.1'
 
